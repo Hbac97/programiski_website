6aea915e11d09f5724abe99b678ac35bfaf0b330
Hbac97 <hubert.baciocha@gmaii.com>
2022-07-07 19:05:44 +0200

    new fresh branch with working sidebar

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory} from 'vue-router'
+import home from '../views/home.vue'
+import about from '../views/about.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-  ],
-});
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: home
+        },
+        {
+            path: '/about',
+            component: about
+        },
 
-export default router;
+
+    ]
+})
+
+
+export default router
