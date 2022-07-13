88736639584d9b57b56690a3823bea272f3ed074
Hublax <hubert.baciocha@gmail.com>
2022-07-13 11:42:31 +0200

    Added terminal

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import shell from 'vue-shell'
 import router from './router'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(shell).mount('#app')
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,20 @@
 <template>
-
+   
     <main class="home">
     </main>
+
+    <div>
+        <v-shell class="terminal mx-auto md:flex"
+        :banner="banner"
+        :shell_input="send_to_terminal"
+        :commands="commands"
+        @shell_output="prompt"
+        ></v-shell>
+    </div>
+
+  <div class= "grid-4">
     <router-link to="/about">
-        <div class="rectangle">
+        <div class="rectangle" >
             <div class="zdj_zakładki">
                 <img src="../assets/example.jpg">
             </div>
@@ -37,7 +48,66 @@
             <div class="tytuł_zakładki">Dołącz do nas!</div>
         </div>
     </router-link>
+    </div>
 </template>
+
+
+<script>
+    export default {
+    data() {
+        return {
+        send_to_terminal: "",
+        banner: {
+            //header: "Programiski",
+            subHeader: "",
+            helpHeader: 'Wpisz "help", poznaj komendy.',
+            emoji: {
+                first: "🐻",
+            },
+            sign: "student $",
+            // img: {
+            //     align: "left",
+            //     link: "../src/assets/logowektor.png",
+            //     width: 100,
+            //     height: 100
+            // }
+        },
+        commands: [
+            { name: "info",
+            get() {
+                return `<p>Witaj na stronie Koła naukowego Programiski i takie tam bla bla bla bla bla blab lalalalalala</p>`;
+            }
+            },
+            {
+            name: "club",
+            get() {
+                return navigator.appVersion;
+            }
+            },
+            {
+            name: "news",
+            get() {
+                return navigator.appVersion;
+            }
+            },
+            {
+            name: "callus",
+            get() {
+                return navigator.appVersion;
+            }
+            }
+        ]
+        };
+    },
+    methods: {
+        prompt(value) {
+        if (value == "node -v") {
+            this.send_to_terminal = process.versions.node;
+        }
+        }
+    }
+    };
+</script>
 
 <style>
   .rectangle {
@@ -73,4 +143,34 @@
     height: 100%;
     border-radius: 25px 25px 25px 25px;
   }
+    #container[data-v-02bdc591]{
+        width: 50%;
+        height: 500px;
+        background: rgb(0, 0, 0, 0.15);
+        border-radius: 30px 30px 30px 30px;
+        display : inline-block;
+        position: relative;
+        margin: 25px;
+        margin-top: 45px;
+        padding-left: 120px;
+        padding-bottom: 120px;
+        left: 65px;
+        font-family: 'Inconsolata', monospace;
+        color: whitesmoke;
+        font-size: 16px;
+        max-height: 500px;
+        max-width: 50%;
+        overflow: hidden;
+        overflow-wrap: break-word;
+    }
+    .prompt[data-v-02bdc591]{
+        color: #A7DEBE;
+    }
+    .cmdline[data-v-02bdc591]{
+        color: whitesmoke;
+        overflow: hidden;
+        overflow-wrap: break-word;
+        
+    }
+
 </style>
